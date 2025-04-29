4843228938d25cb59935a0ce10dce458f87db490
Tyler Camp <tylerc456@gmail.com>
2025-04-28 21:30:08 -0500

    Add customer generation script

---
+++ src/generate_customers.py
@@ -0,0 +1,18 @@
+import faker
+
+fake = faker.Faker()
+
+def generate_customers(n_customers=1000):
+    customers = []
+    for _ in range(n_customers):
+        customer = {
+            "customer_id": fake.uuid4(),
+            "address": fake.address().replace('\n', ', ')
+        }
+        customers.append(customer)
+    return customers
+
+if __name__ == "__main__":
+    customers = generate_customers(5)
+    for c in customers:
+        print(c)
